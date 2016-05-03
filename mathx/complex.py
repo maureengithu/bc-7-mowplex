217ff3c507bbfad59e6cbdc2e1ae3665c3a87aa5
Wambui <maureengithu90@gmail.com>
2016-05-03 10:54:59 +0300

    initialize a complex numbers library

---
+++ mathx/complex.py
@@ -0,0 +1,11 @@
+class Complex(object):
+	'''
+	A complex numbers class function that will
+	perform operations on complex numbers such as;
+		-addition
+		-subtraction
+		-multiplication
+		-division
+	'''
+	def __init__(self,number=""):
+		
